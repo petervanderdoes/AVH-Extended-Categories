162f4171e779a97f87242513b4ed3bee4d80bba0
Peter van der Does <peter@avirtualhome.com>
2009-03-20 14:53:57 -0500

    fix for wp-security-scan

--- widget_extended_categories.php
+++ widget_extended_categories.php
@@ -33,7 +33,7 @@ function widget_extended_categories_init() {
 	function widget_extended_categories($args, $number = 1) {
 		$version = '1.5';
 		// Check for version
-		global $wp_version;
+		require (ABSPATH . WPINC . '/version.php');
 		if ( version_compare($wp_version, '2.5.1', '<') ) {
     		$avh_extcat_canselectcats=false;
     	} else {
@@ -115,7 +115,7 @@ function widget_extended_categories_init() {
 	
 	function widget_extended_categories_control($number = 1) {
 	// Check for version
-		global $wp_version;
+		require (ABSPATH . WPINC . '/version.php');
 		if ( version_compare($wp_version, '2.5.1', '<') ) {
     		$avh_extcat_canselectcats=false;
     	} else {
