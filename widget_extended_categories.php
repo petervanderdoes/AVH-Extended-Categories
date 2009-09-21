37077be5c25935106e96bac64c53deccd7804635
Peter van der Does <peter@avirtualhome.com>
2009-09-21 17:27:32 -0500

    Release of version 2.3.1

--- widget_extended_categories.php
+++ widget_extended_categories.php
@@ -3,7 +3,7 @@
 Plugin Name: Extended Category Widget
 Plugin URI: http://blog.avirtualhome.com/wordpress-plugins
 Description: Replacement of the category widget to allow for greater customization of the category widget.
-Version: 2.3
+Version: 2.3.1
 Author: Peter van der Does
 Author URI: http://blog.avirtualhome.com/
 
