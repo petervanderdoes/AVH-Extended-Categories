c51a6a491879520da77d66387c26ba174d079cc5
Peter van der Does <peter@avirtualhome.com>
2010-01-18 10:30:13 -0500

    Variable typo

--- 2.8/class/avh-ec.core.php
+++ 2.8/class/avh-ec.core.php
@@ -464,7 +464,7 @@ class AVH_EC_Core
 			if ( empty( $r['current_category'] ) && is_category() )
 				$r['current_category'] = $wp_query->get_queried_object_id();
 
-			if ( $hierarchical && (! $selectonly) ) {
+			if ( $hierarchical && (! $selectedonly) ) {
 				$depth = $r['depth'];
 			} else {
 				$depth = - 1; // Flat.
