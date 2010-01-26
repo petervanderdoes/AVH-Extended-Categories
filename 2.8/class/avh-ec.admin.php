93bd971c20ffbb2f196b4a35c21192278aebf639
Peter van der Does <peter@avirtualhome.com>
2010-01-26 06:33:30 -0500

    CSS change for dropdown Let my CSS depend on wp-admin, this makes my CSS override the wp-admin one :)

--- 2.8/class/avh-ec.admin.php
+++ 2.8/class/avh-ec.admin.php
@@ -20,7 +20,7 @@ class AVH_EC_Admin
 
 		// Register Style and Scripts
 		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.dev' : '';
-		wp_register_style( 'avhec-admin-css', AVHEC_PLUGIN_URL . '/inc/avh-ec.admin.css', array (), $this->core->version, 'screen' );
+		wp_register_style( 'avhec-admin-css', AVHEC_PLUGIN_URL . '/inc/avh-ec.admin.css', array ('wp-admin'), $this->core->version, 'screen' );
 
 		add_meta_box( 'groupcat_box_ID', __( 'Category Group', 'avh-ec' ), array (&$this, 'metaboxPostCategoryGroup' ), 'post', 'side', 'core' );
 		add_meta_box( 'groupcat_box_ID', __( 'Category Group', 'avh-ec' ), array (&$this, 'metaboxPostCategoryGroup' ), 'page', 'side', 'core' );
