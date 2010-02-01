da7e342eaac6652b478c013eb29895719936083c
Peter van der Does <peter@avirtualhome.com>
2010-01-31 21:58:48 -0500

    updated javascript, got rid of some warnings

--- 2.8/js/avh-ec.categorygroup.dev.js
+++ 2.8/js/avh-ec.categorygroup.dev.js
@@ -6,14 +6,14 @@ jQuery(document).ready(function($) {
 		for ( o = 0; o < options.length; o++ )
 			if ( id == options[o].value )
 				options[o] = null;
-	}
+	};
 
 	delBefore = function(s) {
 		if ( 'undefined' != showNotice )
 			return showNotice.warn() ? s : false;
 
 		return s;
-	}
+	};
 
 	$('#the-list').wpList({ delBefore: delBefore });
 
