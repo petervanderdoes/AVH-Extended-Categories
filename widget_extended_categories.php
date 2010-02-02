27e075dfa4cbb696edf8eb9ec5c5a6616c06666d
Peter van der Does <peter@avirtualhome.com>
2010-02-01 19:48:22 -0500

    Defines should be quoted

--- widget_extended_categories.php
+++ widget_extended_categories.php
@@ -31,12 +31,12 @@ global $plugin;
 $dir = current( explode( '/', $plugin ) );
 $pluginname = plugin_basename( trim( $plugin ) );
 
-define( AVHEC_PLUGIN_DIR, WP_PLUGIN_DIR . '/' . $dir );
-define( AVHEC_PLUGIN_NAME, $pluginname );
+define( 'AVHEC_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . $dir );
+define( 'AVHEC_PLUGIN_NAME', $pluginname );
 
 if ( ( float ) $wp_version >= 2.8 ) {
-	define( AVHEC_PLUGIN_URL, WP_PLUGIN_URL . '/' . $dir . '/2.8' );
-	define( AVHEC_WORKING_DIR, AVHEC_PLUGIN_DIR . '/2.8' );
+	define( 'AVHEC_PLUGIN_URL', WP_PLUGIN_URL . '/' . $dir . '/2.8' );
+	define( 'AVHEC_WORKING_DIR', AVHEC_PLUGIN_DIR . '/2.8' );
 	require (AVHEC_WORKING_DIR . '/avh-ec.client.php');
 } else {
 	require_once 'widget-pre2.8.php';
