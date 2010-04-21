e4cf39e6f06e6df38b7878f97e0594c07745df23
Peter van der Does <peter@avirtualhome.com>
2010-04-20 19:02:13 -0500

    Used wrong variable

--- 2.8/class/avh-ec.category-group.php
+++ 2.8/class/avh-ec.category-group.php
@@ -251,8 +251,8 @@ class AVH_EC_Category_Group
 	 */
 	function getWidgetTitleForGroup ( $group_id )
 	{
-		if ( isset( $this->db_options_widget_titles[$group_id] ) ) {
-			return ($this->db_options_widget_titles[$group_id]);
+		if ( isset( $this->options_widget_titles[$group_id] ) ) {
+			return ($this->options_widget_titles[$group_id]);
 		}
 		return false;
 	}
