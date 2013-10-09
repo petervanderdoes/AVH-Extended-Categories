11dc536f5ab262804203e80454dc2d9be3f05073
Peter van der Does <github@avirtualhome.com>
2013-10-09 12:01:14 -0400

    Update comment section to confirm with workflow

--- widget_extended_categories.php
+++ widget_extended_categories.php
@@ -1,28 +1,29 @@
 <?php
-/*
-Plugin Name: AVH Extended Categories Widgets
-Plugin URI: http://blog.avirtualhome.com/wordpress-plugins
-Description: Replacement of the category widget to allow for greater customization of the category widget.
-Version: 3.8.0-dev.1
-Author: Peter van der Does
-Author URI: http://blog.avirtualhome.com/
+/**
+ * Plugin Name: AVH Extended Categories Widgets
+ * Plugin URI: http://blog.avirtualhome.com/wordpress-plugins
+ * Description: Replacement of the category widget to allow for greater customization of the category widget.
+ * Version: 3.8.0-dev.1
+ * Author: Peter van der Does
+ * Author URI: http://blog.avirtualhome.com/
+ *
+ * Copyright 2012-2013  Peter van der Does  (email : peter@avirtualhome.com)
 
-Copyright 2012  Peter van der Does  (email : peter@avirtualhome.com)
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 if (! defined('AVH_FRAMEWORK')) {
 	define('AVH_FRAMEWORK', true);
 }
